2513a937bcd46f7d2388c1dd83def76d3af6201d
ImaanJakoet <imaanjakoet1@gmail.com>
2025-11-18 08:07:04 +0000

    made changes to book and library classes

--- Business/Models/Book.cs
+++ Business/Models/Book.cs
@@ -4,16 +4,27 @@ using System;
 namespace LibraryManagementSystem.Models {
     public class Book
     {
-        private int id;
+        //Attributes
+        private int id; //Create unique isbn for each book 
         private string title;
         private string author;
         private int datePublished;
         private string genre;
-        public int Id { get; set; }
+
+        //Getters and Setters
         public string Title { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
         public DateTime PublishedDate { get; set; }
         public List<Genre> Genres { get; set; }
+
+        // Constructor
+        public Book(string title, string author, int datePublished, string genre)
+        {
+            this.title = title;
+            this.author = author;
+            this.datePublished = datePublished;
+            this.genre = genre;
+        }
     }
 }
---
+++ Business/Models/Library.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using LibraryManagementSystem;
+
+namespace LibraryManagementSystem.Business.Models
+{
+    public class Library
+    {
+        private string name;
+        private string address;
+        private string libraryId;
+        private Hash<string, Book> books = new Hash<string, Book>();
+
+        public Library(string libraryId, string name, string address)
+        {
+            this.libraryId = libraryId;
+            this.name = name;
+            this.address = address;
+        }
+
+        public void addBook(Book book)
+        {
+            books.add(book);
+        }
+
+        public void removeBook(Book book)
+        {
+            books.remove(book);
+        }
+
+        public Book searchBook(string bookName)
+        {
+            if (books.contains(bookName))
+            {
+                return books(bookName);
+            }
+            else
+            {
+                console.WriteLine("This library does not have this book");
+            }
+        }
+
+        public void borrowBook(Book book)
+        {
+            book.isAvailable = false;
+            // User's book total increases by 1
+            // Book gets added to user's account
+        }
+    }
+}
